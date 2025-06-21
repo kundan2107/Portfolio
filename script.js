4cd17429f44837db49db2030d714f12b7f79fec3
kundan sharma <kundansh54321@gmail.com>
2025-06-21 23:01:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+// Scroll Sections Active Link
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelector('header nav a');
+
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (to >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+    // Sticky Navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove Toggle Icon Navbar on Scroll
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+}
+
+// Scroll Reveal
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
